2a3bfd82452443c7d0d85f6df2677e40ebffec29
kziarek <01169264@pw.edu.pl>
2023-04-19 18:32:54 +0200

    fixed flickering

--- Lab2/gpio_led_2.py
+++ Lab2/gpio_led_2.py
@@ -6,7 +6,6 @@ import math
 def PWM(gpio, frequency, duty_cycle):
     gpio.export = True
     gpio.direction = 'out'
-    gpio.value = 0
 
     for ii in range(frequency):
         gpio.value = 1
@@ -14,12 +13,14 @@ def PWM(gpio, frequency, duty_cycle):
         gpio.value = 0
         time.sleep(0.01 - duty_cycle/10000)
 
+
     gpio.export = False
 
 
 def main():
     gpio27 = gpio4.SysfsGPIO(27)
-    
+    gpio27.value = 0
+
     for ii in range(100):
         s = (math.sin(ii/10)+1)/2 * 100
         PWM(gpio27, 10, s)
