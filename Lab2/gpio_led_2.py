fb35d24cc860f80c53a92b392dbe7ab946ddccd9
kziarek <01169264@pw.edu.pl>
2023-04-19 18:35:15 +0200

    Moved pin management

--- Lab2/gpio_led_2.py
+++ Lab2/gpio_led_2.py
@@ -4,27 +4,25 @@ from itertools import repeat
 import math
 
 def PWM(gpio, frequency, duty_cycle):
-    gpio.export = True
-    gpio.direction = 'out'
-
     for ii in range(frequency):
         gpio.value = 1
         time.sleep(duty_cycle/10000)
         gpio.value = 0
         time.sleep(0.01 - duty_cycle/10000)
-
-
-    gpio.export = False
+   
 
 
 def main():
     gpio27 = gpio4.SysfsGPIO(27)
+    gpio.export = True
+    gpio.direction = 'out'
     gpio27.value = 0
 
     for ii in range(100):
         s = (math.sin(ii/10)+1)/2 * 100
         PWM(gpio27, 10, s)
 
+     gpio.export = False
 
 if __name__ == "__main__":
     main()
