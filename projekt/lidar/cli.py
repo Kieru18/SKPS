6847a58eac4262318287ead0af47f08382a264cb
jkierucz <01169183@pw.edu.pl>
2023-06-14 13:57:18 +0200

    Replaced initial visualisation with socket oriented plotting

---
+++ projekt/lidar/cli.py
@@ -0,0 +1,62 @@
+import socket
+import math
+from matplotlib import pyplot as plt
+
+
+HOST = "127.0.0.1"
+PORT = 8888
+MAX_ANGLE = 180
+MAX_DIST = 300
+
+
+def calc_coords(angle, distance):
+    x = distance * math.sin(math.radians(angle))
+    y = distance * math.cos(math.radians(angle))
+
+    return x, y
+
+
+def init_plot():
+    plt.ion()
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    plt.title("lidar")W
+    plt.xlabel("distance [mm]")
+    plt.ylabel("distance [mm]")
+    plt.gca().set_aspect('equal')
+
+    axes.set_xlim(-10, MAX_DIST)
+    axes.set_ylim(-MAX_DIST, MAX_DIST)
+    plt.show()
+
+    return fig, ax
+
+
+def update_plot(figure, lines, angle, distance):
+    x, y = calc_coords(angle, distance)
+
+    lines[angle].set_data([0, x], [0, y])
+    lines[angle].set_color('blue')
+
+    figure.canvas.draw_idle()
+    figure.canvas.flush_events()
+
+
+def main():
+    fig, ax = init_plot()
+    lines = [ax.plot([0, 0], [0, 0], '-o')[0] for _ in range(MAX_ANGLE + 1)]
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        while True:
+            data = s.recv(256).decode("utf-8").split(" ")
+            angle = int(data[0])
+            distance = int(data[1])
+
+            update_plot(figure, lines, angle, distance)
+            print(angle, distance)
+
+
+if __name__ == "__main__":
+    main()
