4bce72f0e978f80f8031058d445991e6819aa98b
kziarek <01169264@pw.edu.pl>
2023-06-14 17:48:15 +0200

    added almost-working version of lidar.cpp

--- projekt/lidar/src/lidar.cpp
+++ projekt/lidar/src/lidar.cpp
@@ -1,262 +1,228 @@
+#include <cassert>
+#include <cstdlib>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <strings.h>
+#include <fstream>
+#include <csignal>
+#include <random>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "vl53l0x_api.h"
 #include "vl53l0x_platform.h"
-#include <malloc.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-#define VERSION_REQUIRED_MAJOR 1
-#define VERSION_REQUIRED_MINOR 0
-#define VERSION_REQUIRED_BUILD 1
+constexpr int port = 8888;
+constexpr int min_dc = 500000;
+constexpr int max_dc = 1800000;
+constexpr int pwm_period = 2000000;
 
+int server_socket;
+int client_socket;
+int angle = 0;
+struct sockaddr_in server_address;
+struct sockaddr_in client_address;
+struct sigaction sigIntHandler;
+VL53L0X_Dev_t sensor = {0};
 
-
-void write_data(const std::string &file, const std::string &data) {
-    std::ofstream output;
-    output.open(file);
-    output << text << "\n";
-    output.close();
-}
-
-
-void check_sensor_status(VL53L0X_Error status) {
-    if (status == VL53L0X_ERROR_NONE) {
-        return;
-    }
-    std::cerr << "SENSOR ERROR" << std::endl;
-    exit(-1);
-}
-
-void check_measurement_data(VL53L0X_RangingMeasurementData_t *data) {
-    if (data->RangeStatus == 0) {
-        return;
-    }
-    std::cerr << "SENSOR MEASUREMENT ERROR" << std::endl;
-    exit(-1);
-}
-
-
-
-void print_pal_error(VL53L0X_Error Status){
-    char buf[VL53L0X_MAX_STRING_LENGTH];
-    VL53L0X_GetPalErrorString(Status, buf);
-    printf("API Status: %i : %s\n", Status, buf);
-}
-
-void print_range_status(VL53L0X_RangingMeasurementData_t* pRangingMeasurementData){
-    char buf[VL53L0X_MAX_STRING_LENGTH];
-    uint8_t RangeStatus;
-
-    /*
-     * New Range Status: data is valid when pRangingMeasurementData->RangeStatus = 0
-     */
-
-    RangeStatus = pRangingMeasurementData->RangeStatus;
-
-    VL53L0X_GetRangeStatusString(RangeStatus, buf);
-    printf("Range Status: %i : %s\n", RangeStatus, buf);
-
-}
-
-VL53L0X_Error rangingTest(VL53L0X_Dev_t *pMyDevice)
+void init_socket()
 {
-    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
-    VL53L0X_RangingMeasurementData_t    RangingMeasurementData;
-    int i;
-    FixPoint1616_t LimitCheckCurrent;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+    {
+        std::cerr << ("ERROR opening socket\n");
+        exit(-1);
+    }
+
+    bzero((char *)&server_address, sizeof(server_address));
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(port);
+
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        std::cerr << ("ERROR on binding\n");
+        exit(-1);
+    }
+
+    listen(server_socket, 5);
+    std::cout << "Waiting for connection on port " << port << "\n";
+
+    socklen_t clilen = sizeof(client_address);
+    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &clilen);
+    if (client_socket < 0)
+    {
+        std::cerr << ("ERROR on accept\n");
+        exit(-1);
+    }
+
+    std::cout << "Accepted TCP connection from " << inet_ntoa(client_address.sin_addr) << ":" << ntohs(client_address.sin_port)
+              << "\n";
+}
+
+void send_tcp_message(const std::string &message)
+{
+    int result = write(client_socket, message.c_str(), message.length());
+    if (result < 0)
+    {
+        std::cerr << ("ERROR writing to socket\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void close_socket()
+{
+    std::cout << "Closing socket\n";
+    close(client_socket);
+    close(server_socket);
+}
+
+void check_measurement_data(const VL53L0X_RangingMeasurementData_t &data)
+{
+    if (data.RangeStatus != 0)
+    {
+        std::cerr << "ERROR on measuring distance\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+void write_to_file(const std::string &path, const std::string &text)
+{
+    std::ofstream file;
+    file.open(path);
+    file << text << "\n";
+    file.close();
+}
+
+uint32_t calculate_duty_cycle(const uint32_t &angle)
+{
+    uint32_t duty_cycle_increase = (max_dc - min_dc) * angle / 180;
+    return min_dc + duty_cycle_increase;
+}
+
+void rotate_servo(const uint32_t &angle)
+{
+    std::cout << "Rotating REAL servo to angle: " << angle << "\n";
+
+    uint32_t duty_cycle = calculate_duty_cycle(angle);
+
+    write_to_file("/sys/class/pwm/pwmchip0/pwm0/period", std::to_string(pwm_period));
+    write_to_file("/sys/class/pwm/pwmchip0/pwm0/duty_cycle", std::to_string(duty_cycle));
+    write_to_file("/sys/class/pwm/pwmchip0/pwm0/enable", "1");
+    usleep(150000);
+    write_to_file("/sys/class/pwm/pwmchip0/pwm0/enable", "0");
+}
+
+void check_status(const VL53L0X_Error &sensor_status)
+{
+    if (sensor_status != VL53L0X_ERROR_NONE)
+    {
+        std::cerr << "SENSOR ERROR\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+uint16_t measure_distance(VL53L0X_Dev_t &sensor)
+{
+    VL53L0X_RangingMeasurementData_t measurement_data;
+    VL53L0X_Error status = VL53L0X_PerformSingleRangingMeasurement(&sensor, &measurement_data);
+    check_status(status);
+    check_measurement_data(measurement_data);
+    return measurement_data.RangeMilliMeter;
+}
+
+void init_sensor(VL53L0X_Dev_t &sensor)
+{
+    std::cout << "Initializing REAL VL53L0X sensor\n";
+
     uint32_t refSpadCount;
     uint8_t isApertureSpads;
     uint8_t VhvSettings;
     uint8_t PhaseCal;
+    VL53L0X_Error sensor_status;
 
-    if(Status == VL53L0X_ERROR_NONE)
+    sensor.I2cDevAddr = 0x29;
+    char *i2c_path = strdup("/dev/i2c-1");
+    sensor.fd = VL53L0X_i2c_init(i2c_path, sensor.I2cDevAddr);
+    if (sensor.fd < 0)
     {
-        printf ("Call of VL53L0X_StaticInit\n");
-        Status = VL53L0X_StaticInit(pMyDevice); // Device Initialization
-        print_pal_error(Status);
-    }
-    
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_PerformRefCalibration\n");
-        Status = VL53L0X_PerformRefCalibration(pMyDevice,
-        		&VhvSettings, &PhaseCal); // Device Initialization
-        print_pal_error(Status);
+        std::cerr << "ERROR on initializing sensor\n";
+        exit(EXIT_FAILURE);
     }
 
-    if(Status == VL53L0X_ERROR_NONE) // needed if a coverglass is used and no calibration has been performed
-    {
-        printf ("Call of VL53L0X_PerformRefSpadManagement\n");
-        Status = VL53L0X_PerformRefSpadManagement(pMyDevice,
-        		&refSpadCount, &isApertureSpads); // Device Initialization
-        printf ("refSpadCount = %d, isApertureSpads = %d\n", refSpadCount, isApertureSpads);
-        print_pal_error(Status);
-    }
+    sensor_status = VL53L0X_DataInit(&sensor);
+    check_status(sensor_status);
 
-    if(Status == VL53L0X_ERROR_NONE)
-    {
+    sensor_status = VL53L0X_StaticInit(&sensor);
+    check_status(sensor_status);
 
-        // no need to do this when we use VL53L0X_PerformSingleRangingMeasurement
-        printf ("Call of VL53L0X_SetDeviceMode\n");
-        Status = VL53L0X_SetDeviceMode(pMyDevice, VL53L0X_DEVICEMODE_SINGLE_RANGING); // Setup in single ranging mode
-        print_pal_error(Status);
-    }
+    sensor_status = VL53L0X_PerformRefCalibration(&sensor, &VhvSettings, &PhaseCal);
+    check_status(sensor_status);
 
-    // Enable/Disable Sigma and Signal check
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckEnable(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
-    }
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckEnable(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
-    }
-				
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckValue(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE,
-        		(FixPoint1616_t)(0.25*65536));
-	}			
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckValue(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE,
-        		(FixPoint1616_t)(18*65536));			
-    }
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetMeasurementTimingBudgetMicroSeconds(pMyDevice,
-        		200000);
-    }
-    /*
-     *  Step  4 : Test ranging mode
-     */
+    sensor_status = VL53L0X_PerformRefSpadManagement(&sensor, &refSpadCount, &isApertureSpads);
+    check_status(sensor_status);
 
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        for(i=0;i<10;i++){
-            printf ("Call of VL53L0X_PerformSingleRangingMeasurement\n");
-            Status = VL53L0X_PerformSingleRangingMeasurement(pMyDevice,
-            		&RangingMeasurementData);
+    sensor_status = VL53L0X_SetDeviceMode(&sensor, VL53L0X_DEVICEMODE_SINGLE_RANGING);
+    check_status(sensor_status);
 
-            print_pal_error(Status);
-            print_range_status(&RangingMeasurementData);
-
-            VL53L0X_GetLimitCheckCurrent(pMyDevice,
-            		VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, &LimitCheckCurrent);
-
-            printf("RANGE IGNORE THRESHOLD: %f\n\n", (float)LimitCheckCurrent/65536.0);
-
-
-            if (Status != VL53L0X_ERROR_NONE) break;
-
-            printf("Measured distance: %i\n\n", RangingMeasurementData.RangeMilliMeter);
-
-
-        }
-    }
-    return Status;
+    sensor_status = VL53L0X_StartMeasurement(&sensor);
+    check_status(sensor_status);
 }
 
-int main(int argc, char **argv)
+void export_pwm_chanel()
 {
-    int 
+    write_to_file("/sys/class/pwm/pwmchip0/export", "0");
+}
 
-    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
-    VL53L0X_Dev_t MyDevice;
-    VL53L0X_Dev_t *pMyDevice = &MyDevice;
-    VL53L0X_Version_t                   Version;
-    VL53L0X_Version_t                  *pVersion   = &Version;
-    VL53L0X_DeviceInfo_t                DeviceInfo;
+void unexport_pwm_channel()
+{
+    write_to_file("/sys/class/pwm/pwmchip0/unexport", "0");
+}
 
-    int32_t status_int;
-   
-    printf ("VL53L0X API Simple Ranging example\n\n");
+void handle_interrupt(int signal)
+{
+    std::cout << "\nCaught Ctrl + C\n";
+    rotate_servo(0);
+    unexport_pwm_channel();
+    close_socket();
+    exit(EXIT_SUCCESS);
+}
 
-    // Initialize Comms
-    pMyDevice->I2cDevAddr      = 0x29;
+void init_interrupt()
+{
 
-    pMyDevice->fd = VL53L0X_i2c_init("/dev/i2c-1", pMyDevice->I2cDevAddr);//choose between i2c-0 and i2c-1; On the raspberry pi zero, i2c-1 are pins 2 and 3
-    if (MyDevice.fd<0) {
-        Status = VL53L0X_ERROR_CONTROL_INTERFACE;
-        printf ("Failed to init\n");
-    }
+    sigIntHandler.sa_handler = handle_interrupt;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, nullptr);
+}
 
-    /*
-     *  Get the version of the VL53L0X API running in the firmware
-     */
+int main(int argc, char *argv[])
+{
+    init_interrupt();
+    init_socket();
+    init_sensor(sensor);
 
-    if(Status == VL53L0X_ERROR_NONE)
+    int delta_angle = atoi(argv[1]);
+    assert(delta_angle > 0);
+    uint16_t sleep_time = atoi(argv[2]);
+    uint16_t distance;
+
+    export_pwm_chanel();
+    rotate_servo(0);
+    while (1)
     {
-        status_int = VL53L0X_GetVersion(pVersion);
-        if (status_int != 0)
-            Status = VL53L0X_ERROR_CONTROL_INTERFACE;
+        rotate_servo(angle);
+        distance = measure_distance(sensor);
+        std::string message = std::to_string(angle) + " " + std::to_string(distance) + "\n";
+        std::cout << "Send message: " << message;
+        send_tcp_message(message);
+        angle += delta_angle;
+        if (angle <= 0 || angle >= 180)
+            delta_angle *= -1;
+        usleep(sleep_time);
     }
-
-    /*
-     *  Verify the version of the VL53L0X API running in the firmware
-     */
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        if( pVersion->major != VERSION_REQUIRED_MAJOR ||
-            pVersion->minor != VERSION_REQUIRED_MINOR ||
-            pVersion->build != VERSION_REQUIRED_BUILD )
-        {
-            printf("VL53L0X API Version Error: Your firmware has %d.%d.%d (revision %d). This example requires %d.%d.%d.\n",
-                pVersion->major, pVersion->minor, pVersion->build, pVersion->revision,
-                VERSION_REQUIRED_MAJOR, VERSION_REQUIRED_MINOR, VERSION_REQUIRED_BUILD);
-        }
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_DataInit\n");
-        Status = VL53L0X_DataInit(&MyDevice); // Data initialization
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        Status = VL53L0X_GetDeviceInfo(&MyDevice, &DeviceInfo);
-        if(Status == VL53L0X_ERROR_NONE)
-        {
-            printf("VL53L0X_GetDeviceInfo:\n");
-            printf("Device Name : %s\n", DeviceInfo.Name);
-            printf("Device Type : %s\n", DeviceInfo.Type);
-            printf("Device ID : %s\n", DeviceInfo.ProductId);
-            printf("ProductRevisionMajor : %d\n", DeviceInfo.ProductRevisionMajor);
-            printf("ProductRevisionMinor : %d\n", DeviceInfo.ProductRevisionMinor);
-
-        if ((DeviceInfo.ProductRevisionMinor != 1) && (DeviceInfo.ProductRevisionMinor != 1)) {
-        	printf("Error expected cut 1.1 but found cut %d.%d\n",
-                       DeviceInfo.ProductRevisionMajor, DeviceInfo.ProductRevisionMinor);
-                Status = VL53L0X_ERROR_NOT_SUPPORTED;
-            }
-        }
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        Status = rangingTest(pMyDevice);
-    }
-
-    print_pal_error(Status);
-    
-    // Implementation specific
-
-    /*
-     *  Disconnect comms - part of VL53L0X_platform.c
-     */
-
-    printf ("Close Comms\n");
-    VL53L0X_i2c_close();
-
-    print_pal_error(Status);
-	
-
-    // main loop for getting the measurenments off the lidar  
-    while( )
-
-
-    
-    return (0);
+    return EXIT_SUCCESS;
 }
